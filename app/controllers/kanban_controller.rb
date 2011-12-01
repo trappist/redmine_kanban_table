6e11a9799c4a8551c6d69ba5be4709b05c51199b
Rocco Stanzione <grasshopper@linuxkungfu.org>
2011-12-01 15:18:25 -0600

    Limits

--- app/controllers/kanban_controller.rb
+++ app/controllers/kanban_controller.rb
@@ -7,6 +7,9 @@ class KanbanController < ApplicationController
     @user     = params[:id] ? User.find_by_login(params[:id]) : User.current
     @team     = @user.team
     @manager  = @user.is_manager ? @user : @user.manager
+    @limits   = Hash[*Setting.plugin_kanban_table[:limits].map do |k,v|
+      [k.to_i,(v ? v.to_i : 99999)]
+    end.flatten] rescue {}
     @statuses = IssueStatus.find(Setting.plugin_kanban_table['issue_stati'], :order => "position")
   end
 
