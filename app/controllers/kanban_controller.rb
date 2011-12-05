00d27cbdfb05fe7cb6dcf8bcb943e40a58a7cec5
Rocco Stanzione <grasshopper@linuxkungfu.org>
2011-12-05 12:43:54 -0600

    Use Teams plugin instead of Manager

--- app/controllers/kanban_controller.rb
+++ app/controllers/kanban_controller.rb
@@ -3,11 +3,9 @@ class KanbanController < ApplicationController
   helper :issues
   
   def index
-    @managers = User.find_all_by_is_manager(true)
-    @user     = params[:id] ? User.find_by_login(params[:id]) : User.current
-    @team     = @user.team
-    @manager  = @user.is_manager ? @user : @user.manager
-    @limits   = Hash[*Setting.plugin_kanban_table[:limits].map do |k,v|
+    @teams  = Team.find(:all, :order => :name)
+    @team   = params[:id] ? Team.find(params[:id]) : User.current.teams.first
+    @limits = Hash[*Setting.plugin_kanban_table[:limits].map do |k,v|
       [k.to_i,(v ? v.to_i : 99999)]
     end.flatten] rescue {}
     @statuses = IssueStatus.find(Setting.plugin_kanban_table['issue_stati'], :order => "position")
